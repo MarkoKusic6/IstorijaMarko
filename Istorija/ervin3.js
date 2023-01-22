08e0884356b38167824baf66b2683e27d72e37fe
MarkoKusic6 <123301977+MarkoKusic6@users.noreply.github.com>
2023-01-22 12:46:39 +0100

    IstorijaMarko

---
+++ Istorija/ervin3.js
@@ -0,0 +1,10 @@
+function Promeni(){
+    
+    let marko = document.getElementById("marko");
+    marko.style.fontFamily = "fantasy";
+}
+function Promeni2(){
+     
+    let marko = document.getElementById("marko");
+    marko.style.fontFamily = "Sans-serif";
+}
